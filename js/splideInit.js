6a8d2c7dff5523b881c04957237ae08f7558235c
arsenidze <melihovarsenij@gmail.com>
2023-12-21 22:50:12 +0200

    remove unused files

--- js/splideInit.js
+++
@@ -1,9 +0,0 @@
-// https://splidejs.com/v3/guides/getting-started/
-document.addEventListener('DOMContentLoaded', function () {
-  const splide = new Splide('.splide', {
-    type: 'loop',
-    pagination: false,
-    drag: false,
-  });
-  splide.mount();
-})
